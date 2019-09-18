5fd62eb480a470b36c705e234056f76ebad7a96e
Mark <mdelecate@users.noreply.github.com>
2019-09-18 10:23:39 +0800

    Added cost estimation tooltip
    Duplicated self.estimatedCostString and modified it to return a breakdown of the individual costs as tooltipCostBreakdown. 
    tooltipCostBreakdown is shown during mouseover of the estimated cost.

--- octoprint_costestimation/static/js/costestimation.js
+++ octoprint_costestimation/static/js/costestimation.js
@@ -92,12 +92,75 @@ $(function() {
             return length * Math.PI * radius * radius;
         };
 
+        self.tooltipCostBreakdown = ko.pureComputed(function() {
+            if (!self.showEstimatedCost()) return "-";
+            if (self.printerState.filename() === undefined) return "-";
+            if (self.printerState.filament().length == 0) return "-";
+
+            var pluginSettings = self.settings.settings.plugins.costestimation;
+            var jobFilament =  self.printerState.filament();
+            var spoolData = null;
+
+            if (self.filamentManager !== null && pluginSettings.useFilamentManager()) {
+                spoolData = self.filamentManager.selectedSpools();
+            }
+
+            // calculating filament cost
+            var filamentCost = 0;
+            for (var i = 0; i < jobFilament.length; ++i) {
+                var result = /(\d+)/.exec(jobFilament[i].name()); // extract tool id from name
+                var tool = result === null ? 0 : result[1];
+
+                if (spoolData !== null && spoolData[tool] === undefined) continue;  // skip tools with no selected spool
+
+                var costOfFilament, weightOfFilament, densityOfFilament, diameterOfFilament;
+
+                if (spoolData !== null) {
+                    costOfFilament = spoolData[tool].cost;
+                    weightOfFilament =  spoolData[tool].weight;
+                    densityOfFilament = spoolData[tool].profile.density;
+                    diameterOfFilament = spoolData[tool].profile.diameter;
+                } else {
+                    costOfFilament = parseFloat(pluginSettings.costOfFilament());
+                    weightOfFilament = parseFloat(pluginSettings.weightOfFilament());
+                    densityOfFilament = parseFloat(pluginSettings.densityOfFilament());
+                    diameterOfFilament = parseFloat(pluginSettings.diameterOfFilament());
+                }
+
+                var costPerWeight = weightOfFilament > 0 ? costOfFilament / weightOfFilament : 0;
+                var filamentLength = jobFilament[i].data().length;
+                var filamentVolume = self.calculateVolume(filamentLength, diameterOfFilament) / 1000;
+
+                filamentCost += costPerWeight * filamentVolume * densityOfFilament;
+            }
+
+            // calculating electricity cost
+            var powerConsumption = parseFloat(pluginSettings.powerConsumption());
+            var costOfElectricity = parseFloat(pluginSettings.costOfElectricity());
+            var costPerHour = powerConsumption * costOfElectricity;
+            var estimatedPrintTime = self.printerState.estimatedPrintTime() / 3600;  // h
+            var electricityCost = costPerHour * estimatedPrintTime;
+
+            // calculating printer cost
+            var purchasePrice = parseFloat(pluginSettings.priceOfPrinter());
+            var lifespan = parseFloat(pluginSettings.lifespanOfPrinter());
+            var depreciationPerHour = lifespan > 0 ? purchasePrice / lifespan : 0;
+            var maintenancePerHour = parseFloat(pluginSettings.maintenanceCosts());
+            var printerCost = (depreciationPerHour + maintenancePerHour) * estimatedPrintTime;
+
+            // assembling string
+            var tooltipCostBreakdown = "Filament: "+ filamentCost.toFixed(2) + "Electricity: " + electricityCost.toFixed(2) + "Printer: "+ printerCost.toFixed(2);;
+            return tooltipCostBreakdown;
+        });
+        
+        
         self.onBeforeBinding = function() {
             var element = $("#state").find("hr:nth-of-type(2)");
             if (element.length) {
                 var name = gettext("Cost");
                 var text = gettext("Estimated print cost based on required quantity of filament and print time");
-                element.before("<div id='costestimation_string' data-bind='visible: showEstimatedCost()'><span title='" + text + "'>" + name + "</span>: <strong data-bind='text: estimatedCostString'></strong></div>");
+                var 
+                element.before("<div id='costestimation_string' data-bind='visible: showEstimatedCost()'><span title='" + text + "'>" + name + "</span>: <span title='" + tooltipCostBreakdown + "'><strong data-bind='text: estimatedCostString'></strong></span></div>");
             }
         };
     }
